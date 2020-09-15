dc9a66191274e2cab411423465905ef48b9e2acf
Ariel Reuven Kogan <ar.kogan@gmail.com>
2020-09-15 21:54:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+function theBeatlesPlay(musicians, instruments) {
+  let beatles = []
+  for (let i = 0; i <= 3; i++) {
+    beatles.push(musicians[0], instruments[0])
+  }
+}
