91ba50d10a4cf43e83bee94a0cf7a2f9e95e8fac
Ariel Reuven Kogan <ar.kogan@gmail.com>
2020-09-15 22:06:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function johnLennonFacts(array) {
     "He was a choir boy and boy scout",
     "He hated the sound of his own voice"
   ];
-  while(let i = 0; i < facts.length; i++ ) {
+  while(i < facts.length) {
     array.push(`${facts[i]}!!!`)
   }
   return array
